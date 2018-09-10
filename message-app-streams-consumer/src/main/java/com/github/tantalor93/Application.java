0d4f36ecd35b14c94aab7b5da6b71d9eb714fffc
Tantalor93 <Obenky@gmail.com>
2018-09-10 23:55:36 +0200

    add streams consumer

---
+++ message-app-streams-consumer/src/main/java/com/github/tantalor93/Application.java
@@ -0,0 +1,18 @@
+package com.github.tantalor93;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+    @KafkaListener(topics = "processed-test")
+    public void listen(String message) {
+        System.out.println("Received processed message: " + message);
+    }
+}
