2525c5a2f6271ee131c782ec80240ee22a300c3c
Tantalor93 <Obenky@gmail.com>
2018-09-12 18:39:01 +0200

    improve message logging in consumer

--- message-app-streams-consumer/src/main/java/com/github/tantalor93/Application.java
+++ message-app-streams-consumer/src/main/java/com/github/tantalor93/Application.java
@@ -1,5 +1,8 @@
 package com.github.tantalor93;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -7,12 +10,18 @@ import org.springframework.kafka.annotation.KafkaListener;
 @SpringBootApplication
 public class Application {
 
+    private static Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
 
     @KafkaListener(topics = "processed-test")
-    public void listen(Long message) {
-        System.out.println("Received processed message: " + message);
+    public void listen(final ConsumerRecord<String, Long> record) {
+        logger.info(
+                "Received record with key={} and value={}",
+                record.key(),
+                record.value()
+        );
     }
 }
